82cac4d47b4f70ff427cf368aa8a3e35e2c0e65c
kshaw661 <risk.always@gmail.com>
2023-01-10 22:08:17 +0530

    [FoodOrder-10-01-2023]_created_card

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,37 +1,15 @@
+import Cards from './Cards'
 const Body = () => {
     return (
         <div className="container">
             <div className="row mt-2">
-                <div className="col-4">
-                    <div className="card" >
-                        <img src="..." className="card-img-top" alt="..." />
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
-                            </div>
-                    </div>
-                </div>
-                <div className="col-4">
-                    <div className="card">
-                        <img className="card-img-top" src="..." alt="Card image cap" />
-                        <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
-                            </div>
-                    </div>
-                </div>
-                <div className="col-4">
-                    <div className="card">
-                        <img className="card-img-top" src="..." alt="Card image cap" />
-                        <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="btn btn-primary">Go somewhere</a>
-                            </div>
-                    </div>
-                </div>
+                <Cards />
+                <Cards />
+                <Cards />
+                   
+                <Cards />
+                <Cards />
+                <Cards />
             </div>
         </div>
     );
---
+++ src/components/Cards.js
@@ -0,0 +1,15 @@
+const Cards = () => {
+    return (
+        <div className="col-4">
+            <div className="card" >
+                <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Resturent Name</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">Go AT Aahar</a>
+                    </div>
+            </div>
+        </div>
+    );
+}
+export default Cards;
